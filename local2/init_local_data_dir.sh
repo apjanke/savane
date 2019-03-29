37fb41f4d036c44d18fce326dfd5ebb7cc45bfd6
Andrew Janke <floss@apjanke.net>
2019-03-29 01:18:05 -0400

    Add a script for setting up local data directories

---
+++ local2/init_local_data_dir.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# init_local_data_dir.sh <path>
+#
+# Initializes a data directory hierarchy for a local Savane instance.
+
+root="$1"
+if [[ -z "$root" ]]; then
+	echo &>2 "error: you must supply a directory to create"
+fi
+
+echo "Creating Savane data directories at $root..."
+
+# There's nothing special about this particular dir hierarchy arrangement;
+# it's just a convention that apjanke made up.
+mkdir -p "$root/var"
+mkdir -p "$root/var/uploads"
+mkdir -p "$root/inc"
+mkdir -p "$root/appdatadir"
+mkdir -p "$root/appdatadir/trackers_attachments"
+
+cat <<EOSTR
+Created Savane data directories at $root.
+
+Now add this to your local2/etc-savane/.savane.conf.php file:
+
+\$sys_appdatadir = "$root/appdatadir";
+\$sys_upload_dir = "$root/var/uploads";
+\$sys_trackers_attachments_dir = "\$sys_appdatadir/trackers_attachments";
+
+(Run "make localconf" if local2/etc-savane/.savane.conf.php does not exist.)
+
+EOSTR
